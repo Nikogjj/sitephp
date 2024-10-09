b45897e827154238987a1ac215ee93147a5ed198
niko <nikolaguemmoud@hotmail.fr>
2024-10-09 17:07:23 +0200

    exo

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$notes=[20,10,15,12,9,3];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OKLM</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <p> <?=$a,$age,$b?></p>
+</body>
+</html>
