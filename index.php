6de2316447cf8d7bd2a1addff148f204e299190d
niko <nikolaguemmoud@hotmail.fr>
2024-10-14 09:24:29 +0200

    exo

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
-$notes = [20,10,15,12,9,3];
-for ($i=0; $i < count($notes); $i++) { 
-    $somme=$somme+$notes[$i];
-}
-$somme=$somme/count($notes);
-echo $somme;
+$photo_url = "https://avatars.githubusercontent.com/u/34664499?v=4";
+$eleve = [
+    "name" => "Massinissa",
+    "lastname" => "CHAOUCHI",
+    "age" => 24,
+    "metier" => "Programmeur"
+];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="carte">
+        <p id="titre"><img src=" <?php echo $photo_url?>" alt=""></p>
+        <p>
+            ôfj,fmql,fmlq,ùlq,
+        </p>
+        <p>massi</p>
+        <p>prog</p>
+        <p>24 ans</p>
+    </div>
+</body>
+</html>
